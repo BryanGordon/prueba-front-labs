b21160fbf55eb4bf4fc6f06fc45706a18fd8bae0
BryanGordon <bryanbge92@gmail.com>
2024-07-08 20:14:40 -0500

    Create source template

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const mockWeatherData = {
+    'New York': {
+      temperature: '22°C',
+      humidty: '56%',
+      windSpeed: '15 km/h'
+    },
+    'Los Angeles': {
+      temperature: '27°C',
+      humidty: '45%',
+      windSpeed: '10 km/h'
+    },
+    'London': {
+      temperature: '15°C',
+      humidty: '70%',
+      windSpeed: '20 km/h'
+    }
 
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <input type='text' id='citySearch' placeholder='Search for a city...' />
+      <button id='searchButton'>Search</button>
+      <div id='weatherData'>
+        <div>Temperature: </div>
+        <div>Humidity: </div>
+        <div>Wind Speed: </div>
+        <div>City not found </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='previousSearches'>
+        Component
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
